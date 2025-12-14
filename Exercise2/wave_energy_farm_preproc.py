2a78b99acaf663db52319fb49b5b8a736b0f1ef8
Milic Lénárd <78707856+Lenimao@users.noreply.github.com>
2025-12-14 11:58:39 +0100

    update comparison, and fix in file gen

--- Exercise2/wave_energy_farm_preproc.py
+++ Exercise2/wave_energy_farm_preproc.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
-# Mostly taken from Lenard's exploration notebook. Did not check much if it makes sense or is proper, so should be double checked and fixed.
+# One of the enery datafiles
 
-df = pd.read_csv("data/Steel_industry_data.csv")
+df = pd.read_csv("Exercise2/data/WEC_Perth_49.csv")
 df.head()
-df = df.drop(['date'],axis=1)
 df = df.dropna()
 
 target_col = 'Total_Power'
@@ -20,4 +19,4 @@ result_df = pd.DataFrame(X, columns=num_cols)
 result_df[target_col] = y
 
 
-result_df.to_csv("data/steel_industry_data_preprocessed.csv", index=False)
+result_df.to_csv("Exercise2/data/wave_energy_farm_preprocessed.csv", index=False)
