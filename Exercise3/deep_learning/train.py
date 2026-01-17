3748ac7c309f163587969ef607aaebb194cdeeeb
NicoReit54 <nico.reit54@gmail.com>
2026-01-17 09:43:41 +0100

    Ammended quite a bit to have a loop running through both simple CNN configs

--- Exercise3/deep_learning/train.py
+++ Exercise3/deep_learning/train.py
@@ -2,6 +2,7 @@ import time
 import torch
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from pathlib import Path
 
 # Sources: 
 # https://docs.pytorch.org/tutorials/beginner/introyt/trainingyt.html 
@@ -26,7 +27,7 @@ def train_one_epoch(model, dataloader, optimizer, criterion, device):
         # Zero gradients for every batch: https://stackoverflow.com/questions/48001598/why-do-we-need-to-call-zero-grad-in-pytorch
         # (why? > In Pytorch gradients get accumulated by default due to some convenient
         # handling for other models. If not set Otherwise, the gradient would be a combination of the old gradient, 
-        # which you have already used to update your model parameters and the newly-computed gradient. 
+        # which we would have already used to update our model parameters and the newly-computed gradient. 
         # It would therefore point in some other direction than the intended direction towards the minimum)
         optimizer.zero_grad()
 
@@ -159,13 +160,25 @@ def train_model(model, trainloader, valloader, optimizer, criterion, device, epo
     plt.grid(True)
 
     plt.tight_layout()
-    plt.show()
 
+    # Check if file and dir exist and also append version number
+    Path("cnn_training_curves").mkdir(exist_ok=True)
+
+    file_name = f"cnn_training_curves/training_curves_{model.__class__.__name__}.png"
+
+    base_path = Path(file_name)
+    if base_path.exists():
+        version = 1
+        while (base_path.parent / f"{base_path.stem}_v{version}{base_path.suffix}").exists():
+            version += 1
+        file_name = str(base_path.parent / f"{base_path.stem}_v{version}{base_path.suffix}")
+    
     # Try/catch for saving the figure because its more important to have the model than the plot
     try:
-        plt.savefig("training_curves.png", dpi=300)
+        plt.savefig(file_name, dpi=300)
     except Exception as e:
         print(f"Could not save the figure: {e}")
-
+    
+    plt.show()
 
     return model
